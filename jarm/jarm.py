aa98b5d5be8338130f0ba99a86fa943c478137f8
Nex <nex@nex.sx>
2020-11-19 00:45:36 +0100

    Reinstated copyright notice

--- jarm/jarm.py
+++ jarm/jarm.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2020, salesforce.com, inc.
+# All rights reserved.
+# Licensed under the BSD 3-Clause license.
+# For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+
 import os
 import socket
 import struct
